f7c64a8d1621c1fcaf3635a17e9de0b9c53b5ac6
spreston <sam.preston11@gmail.com>
2021-06-14 17:18:19 -0500

    Basic html & JS variables added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,7 @@
-// Declare varibale for API key
-var apiKey = 'db43bc6aa07a0ca6b3366e8a7eba22c7'
+// Declare variables
+var apiKey = 'db43bc6aa07a0ca6b3366e8a7eba22c7';
+var searchButtonEl = $('#search-button');
+var searchFormEl = $('#search-form');
+var historyEl = $('#history-container');
+var resultsEl = $('#results-container');
+
