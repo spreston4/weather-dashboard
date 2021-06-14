525ed0af2fb795ac9a7e76e8e3c5e31f6a61bc19
spreston <sam.preston11@gmail.com>
2021-06-14 18:11:10 -0500

    Issue retreiving data/displaying data from API fetch.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,30 @@ var searchButtonEl = $('#search-button');
 var searchFormEl = $('#search-form');
 var historyEl = $('#history-container');
 var resultsEl = $('#results-container');
+var city = '';
 
+
+// Function 'getCityApi' to retrieve city info
+function getCityApi(event) {
+
+event.preventDefault();
+
+city = searchFormEl.val();
+console.log(city);
+
+var cityApi = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+
+fetch(cityApi)
+
+    .then(function(response) {
+        return response.json();
+    })
+
+    .then(function(data) {
+        console.log(data.current);
+    })
+
+}
+
+searchButtonEl.click(getCityApi);
+// Function 'renderCity' to display info
