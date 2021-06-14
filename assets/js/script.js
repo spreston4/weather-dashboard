412d8c5b91a49525ed4b4efc0a59705c61649265
spreston <sam.preston11@gmail.com>
2021-06-14 18:30:46 -0500

    Added pseudocode for getForecast function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,27 +7,52 @@ var resultsEl = $('#results-container');
 var city = '';
 
 
-// Function 'getCityApi' to retrieve city info
-function getCityApi(event) {
+// Function 'convertCity' to retrieve city info & convert to lat & long - required data not availble from city search.
+function convertCity(event) {
 
-event.preventDefault();
+    // Stop form reload
+    event.preventDefault();
 
-city = searchFormEl.val();
-console.log(city);
+    // Get city value from search bar
+    city = searchFormEl.val();
+    console.log(city);
 
-var cityApi = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+    // Create fetch URL with new city & apiKey data
+    var cityApi = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
 
-fetch(cityApi)
+    // Fetch URL
+    fetch(cityApi)
 
-    .then(function(response) {
-        return response.json();
-    })
+        .then(function(response) {
+            return response.json();
+        })
 
-    .then(function(data) {
-        console.log(data.current);
-    })
+        .then(function(data) {
+            console.log('City Data: ');
+            console.log(data);
+
+            // Pass coordinate info to the 'getForecast' function
+            getForecast(data.coord);
+
+        })
 
 }
 
-searchButtonEl.click(getCityApi);
-// Function 'renderCity' to display info
+// Function 'getForecast' to get forecast info for selected city coordinates
+function getForecast(obj) {
+
+    // Define variables for lat & long (needed for next API request)
+
+    // Create fetch URL with new lat and long data
+
+    // Fetch URL
+
+    // Pass relevant info to the render function
+    
+}
+
+
+// Function 'renderForecast' to display current forecast data to 'resultsEl'
+
+searchButtonEl.click(convertCity);
+
